6c74e863573d7c4b2a5a01b0f04ec04ad615eef5
zheng-gogo <65955964+zheng-gogo@users.noreply.github.com>
2021-10-14 20:18:30 +0800

    feature:添加、编辑组信息；获取bk用户信息 (#28)
    * feature:添加、编辑组信息；获取bk用户信息

--- home_application/views.py
+++ home_application/views.py
@@ -12,12 +12,20 @@ specific language governing permissions and limitations under the License.
 """
 import time
 
+from django.db import IntegrityError
 from django.http import JsonResponse
 from django.shortcuts import render
 
-
 # 开发框架中通过中间件默认是需要登录态的，如有不需要登录的，可添加装饰器login_exempt
 # 装饰器引入 from blueapps.account.decorators import login_exempt
+from django.utils.datetime_safe import datetime
+from kombu.utils import json
+
+from blueking.component.shortcuts import get_client_by_request
+from home_application.models import Group
+from home_application.utils import check_param
+
+
 def home(request):
     """
     首页
@@ -47,3 +55,50 @@ def send_get_or_post_test(request):
         return JsonResponse({"data": [], "message": f"Get请求发送成功{time.time()}"})
     if request.method == "POST":
         return JsonResponse({"data": [], "message": f"Post请求发送成功{time.time()}"})
+
+
+def add_group(request):
+    """添加组"""
+    req = json.loads(request.body)
+    params = {"name": "组名", "admin": "管理员"}
+    check_result, message = check_param(params, req)
+    if not check_result:
+        return JsonResponse({"result": False, "code": 1, "message": message})
+    name = req.get("name")
+    admin = req.get("admin")
+    try:
+        Group.objects.create(name=name, admin=admin, create_by=request.user.username)
+    except IntegrityError:
+        return JsonResponse({"result": False, "code": 1, "message": "添加失败，组名重复"})
+    else:
+        return JsonResponse({"result": True, "code": 0, "message": "添加成功", "data": []})
+
+
+def update_group(request):
+    """编辑组信息"""
+    req = json.loads(request.body)
+    params = {"id": "组id", "name": "组名", "admin": "管理员"}
+    check_result, message = check_param(params, req)
+    if not check_result:
+        return JsonResponse({"result": False, "code": 1, "message": message})
+    id = req.get("id")
+    name = req.get("name")
+    admin = req.get("admin")
+    try:
+        Group.objects.filter(id=id).update(name=name, admin=admin, update_time=datetime.now())
+    except IntegrityError:
+        return JsonResponse({"result": False, "code": 1, "message": "更新失败，组名重复"})
+    else:
+        return JsonResponse({"result": True, "code": 0, "message": "更新成功", "data": []})
+
+
+def get_all_bk_users(request):
+    """从蓝鲸平台，拉取所有用户列表"""
+    client = get_client_by_request(request=request)
+    response = client.usermanage.list_users(fields="id,username,display_name,email,telephone")
+    result = response.get("result")
+    if result:
+        return JsonResponse({"result": True, "code": 0, "data": response.get("data"), "message": "获取蓝鲸用户列表成功"})
+    else:
+        # 请求接口成功，但获取内容失败，返回错误信息
+        return response
