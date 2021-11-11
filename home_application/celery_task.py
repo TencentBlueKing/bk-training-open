b84fc794644e6183a406b9d281180e8818b6449f
404-error-404 <1791781644@qq.com>
2021-11-11 23:59:51 +0800

    bugfix: 修复补写日报时没有通知管理员的疏漏

--- home_application/celery_task.py
+++ home_application/celery_task.py
@@ -6,7 +6,7 @@ import datetime
 import logging
 
 from celery.schedules import crontab
-from celery.task import periodic_task
+from celery.task import periodic_task, task
 from django.template.loader import get_template
 
 from home_application.models import Daily
@@ -20,9 +20,11 @@ from home_application.utils.mail_operation import send_mail
 logger = logging.getLogger(__name__)
 
 
-# 每天晚上8点发送提醒邮件
 @periodic_task(run_every=crontab(minute=0, hour=20))
 def remind_to_write_daily():
+    """
+    每天晚上8点发送提醒邮件
+    """
     logger.info("定时任务：每晚8点提醒没写日报的同学")
     user_to_notified = ",".join(get_people_not_reported())
     # 都写了日报则无需处理
@@ -58,9 +60,11 @@ def notify_admin_who_not_reported():
         send_mail(receiver__username=mail_info["admins"], title=mail_title, content=mail_content, body_format="Html")
 
 
-# 每天早上10点发送前一天日报，同时告知管理员组内那些同学没交日报
 @periodic_task(run_every=crontab(minute=0, hour=10))
 def send_yesterday_report():
+    """
+    每天早上10点发送前一天日报，同时告知管理员组内那些同学没交日报
+    """
     logger.info("定时任务：每早10点发送前一天所有人的日报")
     yesterday_date = datetime.datetime.today() - datetime.timedelta(days=1)
     notify_info = get_yesterday_reports()
@@ -100,3 +104,38 @@ def send_yesterday_report():
             Daily.objects.filter(id__in=ids).update(send_status=True)
     # 然后告知管理员昨天没写日报的用户
     notify_admin_who_not_reported()
+
+
+@task()
+def send_daily_immediately(user_name, group_admins, daily_content, report_date, report_id):
+    """
+    用户补写日报后立刻发送给管理员
+    :param user_name:       用户名：username(name)
+    :param group_admins:    用户所在组的所有管理员
+    :param daily_content:   日报内容，json数据
+    :param report_date:     日报日期，str
+    :param report_id:       日报id，用于发送后更新状态
+    """
+    mail_title = "日报补写提醒"
+    mail_subhead = "您所管理的组有成员补写了『%s』的日报" % report_date
+    group_reports = [
+        {
+            "report_user": user_name,
+            "report_content": daily_content,
+        }
+    ]
+    html_template = get_template("daily_report.html")
+    mail_content = html_template.render(
+        {"mail_title": mail_title, "mail_subhead": mail_subhead, "group_reports": group_reports}
+    )
+    send_res = send_mail(
+        receiver__username=group_admins,
+        title=mail_title,
+        content=mail_content,
+        body_format="Html",
+    )
+    if send_res["result"]:
+        # 更新日报状态
+        target_report = Daily.objects.get(id=report_id)
+        target_report.send_status = True
+        target_report.save()
