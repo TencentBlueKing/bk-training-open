70d0896a726dbe9d325f10559f4e69592e59cde5
基于人工的bug生成器 <43358330+404-error-404@users.noreply.github.com>
2021-11-27 18:54:21 +0800

    feature: 封装发送昨天日报邮件函数，修改查询昨天日报信息为按组获取 (#120)
    feature: update yesterday daily email

--- home_application/utils/mail_operation.py
+++ home_application/utils/mail_operation.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # @Time     : 2020-07-06 10:24:59
-# @Remarks  :
+# @Remarks  : 邮件相关操作的封装
 import logging
 import os
 
+from django.template.loader import get_template
+
 from blueking.component.shortcuts import get_client_by_user
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("celery")
 
 
-def send_mail(receiver__username, title, content, body_format="Text", attachments=[]):
+def send_mail(receiver__username, title, content, body_format="Text", attachments=None):
     """
     发邮件功能封装
-    :param receiver__username:  接受用户的username
+    :param receiver__username:  接受用户的username，多个用户就以","连接 e.g. 123Q,456Q,789Q
     :param title:               邮件标题
     :param content:             邮件内容
     :param body_format:         邮件格式，默认Text，也可选Html
@@ -21,6 +23,8 @@ def send_mail(receiver__username, title, content, body_format="Text", attachment
     :return:                    API调用结果
     """
     # 从环境变量获取用户名(需添加白名单)
+    if attachments is None:
+        attachments = []
     user = os.getenv("BKAPP_API_INVOKE_USER")
     bk_client = get_client_by_user(user=user)
     # API请求参数
@@ -35,6 +39,36 @@ def send_mail(receiver__username, title, content, body_format="Text", attachment
     if send_result["result"]:
         logger.info(send_result)
     else:
-        error_info = {"邮件状态": "发送邮件失败", "邮件参数": kwargs, "返回结果": send_result}
+        error_info = {"邮件状态": "发送邮件失败", "返回结果": send_result, "邮件参数": kwargs}
         logger.error(error_info)
     return send_result
+
+
+def yesterday_report_notify(notify_title, notify_detail, button_text, button_link, receiver):
+    """
+    发送昨天的日报通知，适用成员的和管理员的
+    :param notify_title:    通知标题
+    :param notify_detail:   渲染通知内容的数据，数据格式为：
+                            [
+                                {"detail": "已完成：", "users": ["username(name)", "12345678Q(小乐乐)"]},
+                                {"detail": "未完成：", "users": ["username(name)""]}
+                            ]
+    :param button_text:     按钮上显示的文字
+    :param button_link:     点击按钮后的跳转链接
+    :param receiver:        邮件接收人，由逗号分割的蓝鲸用户名，e.g. 123Q,456Q,789Q
+    :return:                send_mail()方法的返回值
+    """
+    mail_content = get_template("yesterday_report.html").render(
+        {
+            "notify_title": notify_title,
+            "notify_detail": notify_detail,
+            "button_text": button_text,
+            "button_link": button_link,
+        }
+    )
+    return send_mail(
+        receiver__username=receiver,
+        title=notify_title,
+        content=mail_content,
+        body_format="Html",
+    )
--- home_application/utils/report_operation.py
+++ home_application/utils/report_operation.py
@@ -17,3 +17,30 @@ def content_format_as_json(daily_reports):
     for report in daily_reports:
         report_list.append(report.to_json())
     return report_list
+
+
+def data_to_table(table_style: str, data_style: str, data_list: list, data_count_every_row=3):
+    """
+    将数据构造成HTML邮件模板中的table
+    :param table_style: table的css样式
+    :param data_style: 单元格的css样式
+    :param data_list: 数据列表
+    :param data_count_every_row: table中每行元素的个数
+    :return: 构造好的HTML代码
+    """
+    table_header = '<table class="%s">' % table_style
+    table_end = "</table>"
+
+    table_rows = []
+    table_data = []
+    for index in range(len(data_list)):
+        table_data.append('<td class="{}">{}</td>'.format(data_style, data_list[index]))
+        # 达到指定数量后构造一行table数据
+        if (index + 1) % data_count_every_row == 0:
+            table_rows.append("<tr>\n%s\n</tr>" % "\n".join(table_data))
+            table_data.clear()
+    # 检查table_data是否有遗漏
+    if len(table_data) != 0:
+        table_rows.append("<tr>\n%s\n</tr>" % "\n".join(table_data))
+    table_body = "\n".join(table_rows)
+    return "\n".join([table_header, table_body, table_end])
