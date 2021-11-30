6cdce9013f7673f5348bcc7d178836df408296f1
chenyibo111 <50493482+chenyibo111@users.noreply.github.com>
2021-11-30 23:05:05 +0800

    feature:1.增加管理员页面 2.请求后端接口函数的实现 (#114)
    feature: 前端优化

--- home_application/admin_views.py
+++ home_application/admin_views.py
@@ -49,41 +49,40 @@ def list_member_daily(request, group_id):
     return JsonResponse({"result": True, "code": 0, "message": "", "data": data})
 
 
-@require_http_methods(["GET"])
+@require_http_methods(["POST"])
 def evaluate_daily(request):
     """
     评价组员日报
     """
-    daily_id = request.POST.get("daily_id")
-    evaluate_content = request.POST.get("evaluate")
+    req = json.loads(request.body)
+    daily_id = req.get("daily_id")
+    evaluate_content = req.get("evaluate")
     try:
         evaluate = Daily.objects.get(id=daily_id).evaluate
     except Daily.DoesNotExist:
         return JsonResponse({"result": False, "code": 1, "message": "日报不存在"})
     evaluate.append({"name": request.user.username, "evaluate": evaluate_content})
     Daily.objects.filter(id=daily_id).update(evaluate=evaluate)
-    return JsonResponse({"result": True, "code": 0, "message": "评价成功"})
+    return JsonResponse({"result": True, "code": 0, "message": "点评成功", "data": []})
 
 
 @require_http_methods(["GET"])
 @is_group_member(admin_needed=["GET"])
 def notice_non_report_users(request, group_id):
-    """
-    提醒未写日报成员写日报
-    """
-    req = json.loads(request.body)
-    date = req.get("date")
+    date = request.GET.get("date")
     # 组内成员
     group_user_ids = GroupUser.objects.filter(group_id=group_id).values_list("user_id", flat=True)
     group_users = User.objects.filter(id__in=group_user_ids).values_list("username", flat=True)
     # 填写日报信息
     report_user_usernames = Daily.objects.filter(create_by__in=group_users, date=date).values_list(
-        "username", flat=True
+        "create_by", flat=True
     )
-    non_report_users = group_users.exclude(username__in=report_user_usernames)
-    username_str = ",".join([user.username for user in non_report_users])
+    non_report_users = set(group_users) - set(report_user_usernames)
+    username_str = ",".join(non_report_users)
     # 发送邮件
-    if non_report_users.exists():
+    if non_report_users:
         # 放进celery里
         send_unfinished_dairy.delay(username_str, date)
-    return JsonResponse({"result": True, "code": 0, "message": "一键提醒成功"})
+        return JsonResponse({"result": True, "code": 0, "message": "一键提醒成功", "data": []})
+    else:
+        return JsonResponse({"result": True, "code": 0, "message": "无未写日报成员", "data": []})
