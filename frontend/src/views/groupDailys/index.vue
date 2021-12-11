57cd49bbc1de1e5f3e6573d1a822445673e711cb
616660865@qq.com <616660865@qq.com>
2021-12-11 15:56:28 +0800

    sprintfix:优秀日报需求开发

--- frontend/src/views/groupDailys/index.vue
+++ frontend/src/views/groupDailys/index.vue
@@ -1,8 +1,5 @@
 <template>
     <div class="body">
-        <bk-divider align="left" style="margin-bottom:30px;">
-            <div class="container_title">日报查看</div>
-        </bk-divider>
         <div class="container">
             <div class="left_container">
                 <bk-select :disabled="false" v-model="curGroupId" style="width: 190px;display: inline-block;"
